ec855488b5e1198ab550a7c97015d8aeb7aed214
stomg7969 <stomg7969@gmail.com>
2019-04-21 20:02:32 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,19 @@
 export default function createStore(reducer) {
   // add your code here
+  let state;
+
+  function getState() {
+    return state;
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+  dispatch({ type: "@@init" });
+  return { getState, dispatch };
 }
 
 function render() {
-  const container = document.getElementById('container');
+  const container = document.getElementById("container");
 }
